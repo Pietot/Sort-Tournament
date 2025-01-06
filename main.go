bb19cd8ad5e5bdb5bcacb4c1740111ee04d7589c
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-06 21:10:47 +0100

    Added main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	gosort "gosort/algorithms"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	array := generateUniqueRandomList(100)
+	start := time.Now()
+	gosort.BubbleSortOptimized(array)
+	fmt.Println("Time taken for BubbleSortOptimized: ", time.Since(start))
+
+}
+
+func generateUniqueRandomList(n int) []int {
+	nums := make([]int, n)
+	for i := range nums {
+		nums[i] = i + 1
+	}
+
+	for i := n - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	
+	return nums
+}
