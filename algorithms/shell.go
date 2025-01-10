3615bb6eb5badbb8632aae564f3439d078daf694
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-10 21:48:49 +0100

    Added shell.go

---
+++ algorithms/shell.go
@@ -0,0 +1,18 @@
+package sort
+
+func ShellSort(array []int) []int {
+    arrLength := len(array)
+    gap := arrLength / 2
+	shrink := 2.3
+
+    for gap > 0 {
+        for i := gap; i < arrLength; i++ {
+            for j := i; j >= gap && array[j] < array[j-gap]; j -= gap {
+                array[j], array[j-gap] = array[j-gap], array[j]
+            }
+        }
+        gap = int(float64(gap) / shrink)
+    }
+
+    return array
+}
