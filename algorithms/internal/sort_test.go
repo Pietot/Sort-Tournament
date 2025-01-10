cedaa06ba2a5b2ae9cdbaa52c0cff1136f8910f7
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-10 19:05:05 +0100

    Added sort_test.go

---
+++ algorithms/internal/sort_test.go
@@ -0,0 +1,47 @@
+package sort
+
+import (
+	mySort "gosort/algorithms"
+	"testing"
+)
+
+func TestSorts(test *testing.T) {
+	functions := []struct {
+		name     string
+		function func([]int) []int
+	}{
+		{"BinaryInsertionSort", mySort.BinaryInsertionSort},
+		{"BubbleSortOptimized", mySort.BubbleSortOptimized},
+		{"DoubleSelectionSort", mySort.DoubleSelectionSort},
+		{"ShakerSort", mySort.ShakerSort},
+		{"QuickSort", mySort.QuickSort},
+		{"MergeSort", mySort.MergeSort},
+		{"HeapSort", mySort.HeapSort},
+	}
+
+	unsortedArray := [8]int{5, 3, 8, 6, 2, 7, 1, 4}
+	sortedArray := [8]int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	for _, function := range functions {
+		test.Run(function.name, func(test *testing.T) {
+			result := function.function(unsortedArray[:])
+			if !compareArrays(result, sortedArray) {
+				test.Errorf("Expected %v but got %v", sortedArray, result)
+			}
+		})
+	}
+}
+
+func compareArrays(first_array []int, second_array [8]int) bool {
+	if len(first_array) != len(second_array) {
+		return false
+	}
+
+	for index, value := range first_array {
+		if value != second_array[index] {
+			return false
+		}
+	}
+
+	return true
+}
