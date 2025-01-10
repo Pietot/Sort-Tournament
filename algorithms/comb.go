a3c921051264dec93ad122d58667e4b5affa69a0
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-10 21:48:36 +0100

    Updated comb.go

--- algorithms/comb.go
+++ algorithms/comb.go
@@ -1,24 +1,25 @@
 package sort
 
 func CombSort(array []int) []int {
-    gap := len(array)
-    shrink := 1.3
-    sorted := false
+	gap := len(array)
+	arrLength := len(array)
+	shrink := 1.3
+	sorted := false
 
-    for !sorted {
-        gap = int(float64(gap) / shrink)
-        if gap <= 1 {
-            gap = 1
-            sorted = true
-        }
+	for !sorted {
+		gap = int(float64(gap) / shrink)
+		if gap <= 1 {
+			gap = 1
+			sorted = true
+		}
 
-        for i := 0; i+gap < len(array); i++ {
-            if array[i] > array[i+gap] {
-                array[i], array[i+gap] = array[i+gap], array[i]
-                sorted = false
-            }
-        }
-    }
+		for offset := 0; offset+gap < arrLength; offset++ {
+			if array[offset] > array[offset+gap] {
+				array[offset], array[offset+gap] = array[offset+gap], array[offset]
+				sorted = false
+			}
+		}
+	}
 
-    return array
+	return array
 }
