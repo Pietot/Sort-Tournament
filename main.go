ce095e41ce01ecebadc268da5943421e1e70d29c
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-11 20:41:00 +0100

    Updated main.go

--- main.go
+++ main.go
@@ -13,47 +13,51 @@ func main() {
 
 	start := time.Now()
 	sort.Ints(append([]int(nil), array...))
-	fmt.Println("Time taken for sort.Ints:", time.Since(start))
+	fmt.Printf("Time taken for sort.Ints: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.InsertionSort(append([]int(nil), array...))
-	fmt.Println("Time taken for InsertionSort:", time.Since(start))
+	fmt.Printf("Time taken for InsertionSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.BinaryInsertionSort(append([]int(nil), array...))
-	fmt.Println("Time taken for BinaryInsertionSort:", time.Since(start))
+	fmt.Printf("Time taken for BinaryInsertionSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.BubbleSortOptimized(append([]int(nil), array...))
-	fmt.Println("Time taken for BubbleSort optimized:", time.Since(start))
+	fmt.Printf("Time taken for BubbleSort optimized: %.3f ms\n", time.Since(start).Seconds()*1000)
+
+	start = time.Now()
+	mySort.SelectionSort(append([]int(nil), array...))
+	fmt.Printf("Time taken for SelectionSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.DoubleSelectionSort(append([]int(nil), array...))
-	fmt.Println("Time taken for DoubleSelectionSort:", time.Since(start))
+	fmt.Printf("Time taken for DoubleSelectionSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.ShakerSort(append([]int(nil), array...))
-	fmt.Println("Time taken for ShakerSort:", time.Since(start))
+	fmt.Printf("Time taken for ShakerSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.QuickSort(append([]int(nil), array...))
-	fmt.Println("Time taken for QuickSort:", time.Since(start))
+	fmt.Printf("Time taken for QuickSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.MergeSort(append([]int(nil), array...))
-	fmt.Println("Time taken for MergeSort:", time.Since(start))
+	fmt.Printf("Time taken for MergeSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.HeapSort(append([]int(nil), array...))
-	fmt.Println("Time taken for HeapSort:", time.Since(start))
+	fmt.Printf("Time taken for HeapSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.CombSort(append([]int(nil), array...))
-	fmt.Println("Time taken for CombSort:", time.Since(start))
+	fmt.Printf("Time taken for CombSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 
 	start = time.Now()
 	mySort.ShellSort(append([]int(nil), array...))
-	fmt.Println("Time taken for ShellSort:", time.Since(start))
+	fmt.Printf("Time taken for ShellSort: %.3f ms\n", time.Since(start).Seconds()*1000)
 }
 
 func generateUniqueRandomList(n int) []int {
