9f13a017ef13d1ddda4270f741ab46ab75c662ff
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-23 01:12:57 +0100

    Updated couting_unstable.go

--- algorithms/counting_unstable.go
+++ algorithms/counting_unstable.go
@@ -1,22 +1,22 @@
 package sort
 
 func CountingSortUnstable(array []int) []int {
-    max := getMax(array)
-    count := make([]int, max+1)
-    output := make([]int, len(array))
+	max := getMax(array)
+	count := make([]int, max+1)
 
-    for _, val := range array {
-        count[val]++
-    }
+	for _, val := range array {
+		count[val]++
+	}
 
-    index := 0
-    for i, c := range count {
-        for c > 0 {
-            output[index] = i
-            index++
-            c--
-        }
-    }
+	index := 0
 
-    return output
+	for i, c := range count {
+		for c > 0 {
+			array[index] = i
+			index++
+			c--
+		}
+	}
+    
+	return array
 }
